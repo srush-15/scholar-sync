2fdbc6715edf461d7a00f386017a6eb658a97d11
Nikunj <nikunjmi123@gmail.com>
2024-07-16 12:37:53 +0530

    added refresh token controller and route with refresh token rotation

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,11 +1,13 @@
 import Router from "express";
-import { registerUser, loginUser,logoutUser } from "../controllers/user.controllers.js";
+import { registerUser, loginUser,logoutUser, refreshAccessToken } from "../controllers/user.controllers.js";
 import verifyJWT from "../middlewares/auth.middleware.js";
 const router = Router();
 
 router.route("/signup").post(registerUser);
 
 router.route("/login").post(loginUser);
+
+router.route("/refreshToken").post(refreshAccessToken);
 
 router.route("/logout").post(verifyJWT,logoutUser);
 
