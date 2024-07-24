8d9d5f1ce7a9fb3578e265438ec17414eb4fdd88
Nikunj <nikunjmi123@gmail.com>
2024-07-24 22:22:22 +0530

    added controllers for creating rooms,sections and meeting times

--- server/controllers/meetingTime.controllers.js
+++ server/controllers/meetingTime.controllers.js
@@ -1,3 +1,38 @@
-const addMeetingTime = () => {};
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { MeetingTime } from "../db/meetingTime.model.js";
+const addMeetingTime = async (req, res) => {
+  try {
+    const { timeId, startTime, endTime, day } = req.body;
+    if (!timeId || !startTime || !endTime || !day) {
+      res
+        .status(400)
+        .json(new ApiResponse(false, {}, "All fields are required"));
+      return;
+    }
+    const existingMeetTiming = await MeetingTime.findOne({ timeId });
+    if (existingMeetTiming) {
+      res
+        .status(400)
+        .json(new ApiResponse(false, {}, "Meeting Time already exists"));
+      return;
+    }
+    const meeting = await MeetingTime.create({
+      timeId,
+      startTime,
+      endTime,
+      day,
+    });
+    if(!meeting){
+        res.status(500).json(new ApiResponse(false, {}, "Failed to create new meeting time"));
+        return
+    }
+    res.status(200).json(new ApiResponse(true,meeting,"Meeting time successfully created"))
+  } catch (error) {
+    console.log("An error happened while creating the meeting time", error);
+    res
+      .status(500)
+      .json(new ApiResponse(false, {}, "Failed to create new meeting time"));
+  }
+};
 
 export { addMeetingTime };
--- server/controllers/section.controllers.js
+++ server/controllers/section.controllers.js
@@ -1,3 +1,37 @@
-const addSection = () => {};
+import { ApiResponse } from "../utils/ApiResponse.js";
+import {Section} from "../db/section.model.js"
+import {Department} from "../db/department.model.js"
+const addSection = async (req,res) => {
+    try {
+        const {sectionId , department} = req.body;
+        if(!sectionId || !department){
+            res.status(400).json(new ApiResponse(false,{},"All fields are required"));
+            return;
+        }
+        const existingSection = await Section.findOne({sectionId});
+        if(existingSection){
+            res.status(400).json(new ApiResponse(false,{},"Section already exists"));
+            return;
+        }
+        const sectionsDepartment = await Department.findOne({departmentName :department})
+        if(!sectionsDepartment){
+            res.status(400).json(new ApiResponse(false,{},"Department does not exists"));
+            return;
+        }
+        const section = await Section.create({
+            sectionId, department:sectionsDepartment._id
+        })
+        if(!section){
+            res.status(500).json(new ApiResponse(false,{},"Failed to create new section"));
+            return;
+        }
+        res.status(200).json(new ApiResponse(true,section,"Section created successfully"));
+    } catch (error) {
+        console.log("An error happened while creating the section ", error);
+        res
+        .status(500)
+        .json(new ApiResponse(false, {}, "Failed to create new section"));
+    }
+};
 
 export { addSection };
